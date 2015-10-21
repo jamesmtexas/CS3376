f0e3d5d9a1b4a80451771b670cd1b461254e632a
James Murphy <jmm140330@utdallas.edu>
2015-10-20 20:03:46 -0500

    First commit

---
+++ parser.c
@@ -0,0 +1,6 @@
+#include "parser.h"
+
+//call exec from here? need to make sense of the tokens like pipe
+//create pipe function using dup2
+void parse(char **tokens) {
+}
---
+++ tokenizer.c
@@ -0,0 +1,28 @@
+#include "tokenizer.h"
+
+void tokenize(char *input, char output[][]) {
+
+	int i=0;
+	int j=0;
+
+	for(char *iterator = input; iterator!=0; iterator++) {
+		if (*iterator == ' ') {
+			output[i][j] = 0;
+			i++;
+		}
+		else if(*iterator == '|') {
+			//pipe
+		}
+		else if(*iterator == '>' || '<') {
+			//redirect
+		}
+		else if(*iterator == ';') {
+			//end statement
+		}
+		else {
+			output[i][j] = *iterator;
+		}
+
+		j++;
+	}
+}
